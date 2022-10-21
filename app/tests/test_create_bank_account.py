9ef3a4e83c5f4ca909042770810fa3033c599971
GGryg <shiryon@wp.pl>
2022-10-21 15:20:37 +0200

    feature 3

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,12 +3,15 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
-
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    PESEL = "12345678901"
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", "123456789801")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.PESEL)
+        self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.PESEL, "123456789801", "PESEL nie został zapisany")
+        self.assertEqual(pierwsze_konto.PESEL, self.PESEL, "PESEL nie został zapisany")
+        self.assertEqual(len(pierwsze_konto.PESEL), 11, "Nie prawidłowa długość PESELa")
 
     #tutaj proszę dodawać nowe testy
