a62bf5ee8f5c05fac00a5e48ab609a5df15a4a62
GGryg <shiryon@wp.pl>
2022-10-21 15:12:57 +0200

    feature_2 poprawka

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,10 +5,10 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski",198298743)
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "123456789801")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.PESEL, 198298743, "PESEL nie został zapisany")
+        self.assertEqual(pierwsze_konto.PESEL, "123456789801", "PESEL nie został zapisany")
 
     #tutaj proszę dodawać nowe testy
