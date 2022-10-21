cbe3e15ac8ed2c506f2220290621d88383a5d774
GGryg <shiryon@wp.pl>
2022-10-21 15:30:11 +0200

    początek feature 4

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,6 +6,7 @@ class TestCreateBankAccount(unittest.TestCase):
     imie = "Dariusz"
     nazwisko = "Januszewski"
     PESEL = "12345678901"
+    kod = "PROM_XYZ"
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto(self.imie, self.nazwisko, self.PESEL)
         self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
@@ -15,3 +16,11 @@ class TestCreateBankAccount(unittest.TestCase):
         self.assertEqual(len(pierwsze_konto.PESEL), 11, "Nie prawidłowa długość PESELa")
 
     #tutaj proszę dodawać nowe testy
+    def test_tworzenie_konta_z_kodem(self):
+        konto_z_kodem = Konto(self.ime, self.nazwisko, self.PESEL, self.kod)
+        self.assertEqual(konto_z_kodem.imie, self.imie, "Imie nie zostało zapisane!")
+        self.assertEqual(konto_z_kodem.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
+        self.assertEqual(konto_z_kodem.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(konto_z_kodem.PESEL, self.PESEL, "PESEL nie został zapisany")
+        self.assertEqual(len(konto_z_kodem.PESEL), 11, "Nie prawidłowa długość PESELa")
+        self.assertEqual(konto_z_kodem.kod[0:4], "PROM_", "Nie ma początku PROM_")
